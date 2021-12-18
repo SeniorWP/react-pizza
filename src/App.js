751bfdfaf23d714b893c8e659e76bf20f698bb24
SeniorWP <sseniorr14@gmail.com>
2021-12-18 19:27:27 +0300

    2 часть

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Header from './Header';
-import Button from './Button';
+import { Header, Categories } from './components';
+
 
 function App() {
   return (
@@ -10,16 +10,7 @@ function App() {
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <div className="categories">
-              <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-              </ul>
-            </div>
+            <Categories onClickItem={(name) => console.log(name)} items={['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']} />
             <div className="sort">
               <div className="sort__label">
                 <svg
