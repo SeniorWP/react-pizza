34235b32524d83f31a93b0957587704965219da4
SeniorWP <sseniorr14@gmail.com>
2021-12-23 10:20:45 +0300

    часть 9 (добавил корзину)

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import classNames from 'classnames'
 import PropTypes from 'prop-types'
+import Button from '../Button'
 
-import LoadingBlock from './LoadingBlock'
-
-function PizzaBlock({ name, imageUrl, price, types, sizes }) {
+function PizzaBlock({ id, name, imageUrl, price, types, sizes, onClickAddPizza, addedCount }) {
 
   const aviableTypes = ['тонкое', 'традиционное']
   const aviableSizes = [26, 30, 40]
   const [activeType, setActiveType] = React.useState(types[0])
-  const [activeSize, setActiveSize] = React.useState(sizes[0])
+  const [activeSize, setActiveSize] = React.useState(0)
 
   const onSelectType = (index) => {
     setActiveType(index)
@@ -17,6 +16,18 @@ function PizzaBlock({ name, imageUrl, price, types, sizes }) {
 
   const onSelectSize = (index) => {
     setActiveSize(index)
+  }
+
+  const onAddPizza = () => {
+    const obj = {
+      id,
+      name,
+      imageUrl,
+      price,
+      type: aviableTypes[activeType],
+      size: aviableSizes[activeSize],
+    }
+    onClickAddPizza(obj)
   }
 
   return (
@@ -37,7 +48,7 @@ function PizzaBlock({ name, imageUrl, price, types, sizes }) {
       </div>
       <div className="pizza-block__bottom">
         <div className="pizza-block__price">от {price} р</div>
-        <div className="button button--outline button--add">
+        <Button onClick={onAddPizza} className="button--add" outline>
           <svg
             width="12"
             height="12"
@@ -50,8 +61,8 @@ function PizzaBlock({ name, imageUrl, price, types, sizes }) {
             />
           </svg>
           <span>Добавить</span>
-          <i>2</i>
-        </div>
+          {addedCount && <i>{addedCount}</i>}
+        </Button>
       </div>
     </div>
   )
@@ -63,6 +74,8 @@ PizzaBlock.propTypes = {
   price: PropTypes.number,
   types: PropTypes.arrayOf(PropTypes.number),
   sizes: PropTypes.arrayOf(PropTypes.number),
+  onClickAddPizza: PropTypes.func,
+  addedCount: PropTypes.number,
 }
 
 PizzaBlock.defaultProps = {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Categories, SortPopup, PizzaBlock, PizzaLoadingBlock } from '../compone
 
 import { setCategory, setSortBy } from '../redux/actions/filters'
 import { fetchPizzas } from '../redux/actions/pizzas'
+import { addPizzaToCart } from '../redux/actions/cart'
 
 const categoryNames = ['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
 const sortItems = [{ name: 'популярности', type: 'popular', order: 'desc' }, { name: 'цене', type: 'price', order: 'desc' }, { name: 'алфавиту', type: 'name', order: 'asc' },]
@@ -13,6 +14,7 @@ const sortItems = [{ name: 'популярности', type: 'popular', order: '
 function Home() {
   const dispatch = useDispatch()
   const items = useSelector(({ pizzas }) => pizzas.items)
+  const cartItems = useSelector(({ cart }) => cart.items)
   const isLoaded = useSelector(({ pizzas }) => pizzas.isLoaded)
   const { category, sortBy } = useSelector(({ filters }) => filters)
 
@@ -28,6 +30,10 @@ function Home() {
     dispatch(setSortBy(type))
   }, [])
 
+  const handleAddPizzaToCart = obj => {
+    dispatch(addPizzaToCart(obj))
+  }
+
   return (
     <div className="container">
       <div className="content__top">
@@ -37,7 +43,7 @@ function Home() {
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
         {isLoaded ?
-          items.map(obj => <PizzaBlock key={obj.id} isLoading={true} {...obj} />) : Array(12).fill('').map((_, index) => <PizzaLoadingBlock key={index} />)
+          items.map(obj => <PizzaBlock onClickAddPizza={handleAddPizzaToCart} key={obj.id} addedCount={cartItems[obj.id] && cartItems[obj.id].length} {...obj} />) : Array(12).fill('').map((_, index) => <PizzaLoadingBlock key={index} />)
         }
       </div>
     </div>
